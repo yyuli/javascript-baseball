5ba068a1e4d6c64e3a649fca26fd5bebeb3b1420
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-10-01 01:27:10 +0900

    feat: 랜덤 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+  }
+  makeRandomNumbers() {
+    const randomNumbers = [];
+    while(randomNumbers.length < 3) {
+      const pickNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      if(!randomNumbers.includes(pickNumber)) randomNumbers.push(pickNumber);
+    }
+  }
 }
 
 module.exports = App;
