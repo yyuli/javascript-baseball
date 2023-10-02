ac871ca0d71ca93ee0a3ca1ba701724c7d1be601
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-10-02 20:49:03 +0900

    feat: play 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
+  constructor() {
+    this.randomNumbers = [];
+  }
   play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.makeRandomNumbers();
   }
   makeRandomNumbers() {
-    const randomNumbers = [];
-    while(randomNumbers.length < 3) {
+    while(this.randomNumbers.length < 3) {
       const pickNumber = MissionUtils.Random.pickNumberInRange(1, 9);
-      if(!randomNumbers.includes(pickNumber)) randomNumbers.push(pickNumber);
+      if(!this.randomNumbers.includes(pickNumber)) this.randomNumbers.push(pickNumber);
     }
   }
 }
