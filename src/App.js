74640556c2dd2bc5702adc5f80b2a8d81a7a59f8
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-10-03 15:41:48 +0900

    feat: 게임 시작 함수 생성

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   constructor() {
     this.randomNumbers = [];
+    this.inputNumbers = [];
   }
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     this.makeRandomNumbers();
+    this.startGame();
   }
   makeRandomNumbers() {
     const randomNumbers = [];
@@ -15,6 +17,11 @@ class App {
       if(!randomNumbers.includes(pickNumber)) randomNumbers.push(pickNumber);
     }
     this.randomNumbers = randomNumbers;
+  }
+  startGame() {
+    MissionUtils.Console.readLine('숫자를 입력해 주세요. : ', (input) => {
+      this.inputNumbers = input.split('').map(num => parseInt(num));
+    })
   }
 }
 
